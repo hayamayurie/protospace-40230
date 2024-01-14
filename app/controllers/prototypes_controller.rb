525b5dea6e118f0124dfd224ad4d6c7e033b0a30
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-01-15 05:02:01 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(image_params)
+    if @prototype.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(image_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+
+end
+
+private
+
+def image_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
+end
+
+def comment_params
+  params.require(:comment).permit(:content).merge(user_id: current_user.id,)
+end
+
+
+def move_to_index
+  @prototype = Prototype.find(params[:id])
+  if @prototype.user_id != current_user.id
+    redirect_to action: :index
+  end
+end
